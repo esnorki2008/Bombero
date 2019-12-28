0ec58d3b7e9fce88237ee36847b7afb1c9c5c350
esnorki2008 <49577095+esnorki2008@users.noreply.github.com>
2019-12-28 12:10:23 -0600

    1

--- Bombero/src/Elementos/Bomba.java
+++ Bombero/src/Elementos/Bomba.java
@@ -13,11 +13,12 @@ import java.util.logging.Logger;
  * @author 50241
  */
 public class Bomba extends Entidad{
-
     public Bomba(int Vida, int X, int Y, int Ataque, Tablero Tabla) {
         super(Vida, X, Y, Ataque, Tabla);
     }
-    
+    public Bomba(int Vida, int X, int Y, int Ataque, Tablero Tabla,int Rango) {
+        super(Vida, X, Y, Ataque, Tabla,Rango);
+    }
   
 
     @Override
@@ -37,11 +38,26 @@ public class Bomba extends Entidad{
     private void Explotar(){
         //Daña En Forma de cruz
         // Y Estatica X Variable
-        for(int i=0;i<12;i++){
+        for(int i=super.X;i<super.X+Rango;i++){
+            if(i>11)
+                break;
+           Tabla.DañarEntidad(i, Y,super.Ataque);
+        }
+        for(int i=super.X-Rango;i<=super.X;i++){
+            if(i<0)
+                break;
            Tabla.DañarEntidad(i, Y,super.Ataque);
         }
         // X Estatica Y Variable
-        for(int j=0;j<12;j++){
+        
+        for(int j=super.Y;j<super.Y+Rango;j++){
+            if(j>11)
+                break;
+            Tabla.DañarEntidad(X, j, super.Ataque);
+        }
+        for(int j=super.Y-Rango;j<=super.Y;j++){
+            if(j<0)
+                break;
             Tabla.DañarEntidad(X, j, super.Ataque);
         }
     }
